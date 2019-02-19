2df4ec5d95f847fb4b6c223491c9c6671f951c0c
michaelJustin <michael.justin@habarisoft.com>
2019-02-19 10:16:12 +0100

    resolves #10

--- examples/formtests/FormTests.dpr
+++ examples/formtests/FormTests.dpr
@@ -1,14 +1,13 @@
 program FormTests;
 
 uses
-  OpenCTF, OpenCTFRunner, ctfStandardTests,
+  OpenCTF, ctfStandardTests,
+  GUITestRunner,
   TestForm in 'TestForm.pas' {Form1};
 
 begin
-  // register form classes
   OpenCTF.RegisterFormClasses([TForm1]);
 
-  // run the tests
-  OpenCTFRunner.Run;
+  RunRegisteredTests;
 end.
 
--- examples/frametests/TestFrames.dpr
+++ examples/frametests/TestFrames.dpr
@@ -1,16 +1,15 @@
 program TestFrames;
 
 uses
-  OpenCTF, OpenCTFRunner, ctfStandardTests,
+  OpenCTF, ctfStandardTests,
+  GUITestRunner,
   Unit1 in 'Unit1.pas' {Form1},
   Unit2 in 'Unit2.pas' {Frame2: TFrame};
 
 {$R *.res}
 
 begin
-  // register form and frame classes
   OpenCTF.RegisterFormClasses([TForm1, TFrame2]);
 
-  // run the tests
-  OpenCTFRunner.Run;
+  RunRegisteredTests;
 end.
